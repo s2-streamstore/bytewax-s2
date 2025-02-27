d8b70d8633e853138966a1033bc88792a1544509
quettabit <dwarak@s2.dev>
2025-02-26 17:48:03 -0700

    initial commit

--- tests/sink.py
+++ tests/sink.py
@@ -1,23 +1,33 @@
 import os
-import bytewax.operators as op
-from bytewax.testing import TestingSource, run_main
-from bytewax.dataflow import Dataflow
-from bytewax_s2 import S2Config, S2SinkRecord, S2Sink
 
-AUTH_TOKEN = os.getenv("S2_AUTH_TOKEN")
-BASIN = os.getenv("S2_BASIN")
-STREAM_PREFIX = os.getenv("S2_STREAM_PREFIX")
+import bytewax.operators as op
+from bytewax.dataflow import Dataflow
+from bytewax.testing import TestingSource, run_main
+
+from bytewax_s2 import S2Config, S2Sink, S2SinkRecord
+
+AUTH_TOKEN = os.environ["S2_AUTH_TOKEN"]
+BASIN = os.environ["S2_BASIN"]
+STREAM_PREFIX = os.environ["S2_STREAM_PREFIX"]
 
 eof = 0
 
-flow = Dataflow("s2-sink-test")
-ints = op.input("input", flow, TestingSource([1, 2, 3, 4, 5, TestingSource.EOF]))
+
+def key(i: object) -> tuple[str, S2SinkRecord]:
+    if isinstance(i, int):
+        return (str(i % 2), S2SinkRecord(body=i.to_bytes(32)))
+    elif isinstance(i, TestingSource.EOF):
+        return (str(eof % 2), S2SinkRecord(body=eof.to_bytes(32)))
+    else:
+        raise RuntimeError(f"Unexpected type: {type(i)} for input: {i}")
+
+
+flow = Dataflow("s2_sink_test")
+ints = op.input("input", flow, TestingSource([1, 2, 3, 4, 5, TestingSource.EOF()]))
 keyed_ints = op.map(
-    "keying",
+    "key",
     ints,
-    lambda i: (str(i % 2), S2SinkRecord(body=i.to_bytes(32)))
-    if i != TestingSource.EOF
-    else (str(eof % 2), S2SinkRecord(body=eof.to_bytes(32))),
+    key,
 )
 op.output(
     "output",
